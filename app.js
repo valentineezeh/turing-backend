381141e2f1df4da6357c2e4c761a08f692d9735d
valentineezeh <val.ezeh15@gmail.com>
2019-05-31 05:41:06 +0100

    Title: set up base app - Set up and configure base application. - Set up express server. - Set up sequelise and database configuration.

---
+++ app.js
@@ -0,0 +1,66 @@
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import errorhandler from 'errorhandler';
+
+dotenv.config();
+
+const isProduction = process.env.NODE_ENV === 'production';
+// Create global app Objects
+const app = express();
+// Set app to use cors
+app.use(cors());
+
+// Normal express config defaults
+app.use(require('morgan')('dev'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+if (!isProduction) {
+  app.use(errorhandler());
+}
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+
+if (!isProduction) {
+  // eslint-disable-next-line no-unused-vars
+  app.use((err, req, res, next) => {
+    // eslint-disable-next-line no-console
+    console.log(err.stack);
+
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err
+      }
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    errors: {
+      message: err.message,
+      error: {}
+    }
+  });
+});
+
+export default app;
---
+++ src/bin/www.js
@@ -0,0 +1,40 @@
+import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
+import app from '../../app';
+
+dotenv.config();
+
+// get the host and port name
+const hostName = process.env.HOSTNAME || 'localhost';
+const port = process.env.PORT || 3000;
+
+// Connect sequelise to the database
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PSWD, {
+  host: process.env.DB_URL, // your server
+  dialect: process.env.DB_DIALECT,
+  logging: console.log,
+  define: {
+    timestamps: false
+  }
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log('connection has been established.');
+  }).catch((err) => {
+    // eslint-disable-next-line no-console
+    console.error('Unable to connect to the database: ', err);
+  });
+
+// finally, let's start our server...
+const server = app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Listening on ${hostName}: ${server.address().port}`);
+});
+
+process.on('SIGINT', () => {
+  sequelize.close(); // This close the connection to the database
+  process.exit(0);
+});
